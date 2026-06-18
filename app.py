024a9975e9e44a4864d72133c6f097aae1c0239f
therealsammy30 <therealsammy30@gmail.com>
2026-06-18 13:19:25 -0700

    Added custom app titles

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+st.markdown("<h3 style='font-family: Arial, sans-serif; color: #374151; margin-bottom: 0px;'>🛡️ Cupertino Ready</h3>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-family: Arial, sans-serif; color: #1f2937; margin-top: 10px;'>Prepare together. Stay safe together.</h1>", unsafe_allow_html=True)
