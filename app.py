bfca5f67ab010adc8be7259e979e5359ff73644c
ahanagrandhi-ai <ahana.grandhi@gmail.com>
2026-07-07 11:24:00 -0700

    Completed second row of cupertino zones

--- app.py
+++ app.py
@@ -29,3 +29,16 @@ with zone_col2:
     st.markdown("<p style='font-family: Arial, sans-serif; color: #4b5563; margin-top: 5px;'>Areas that are adjacent to Stevens Creek and the surrounding open space. With dense dry brush and limited escape routes, this area of Cupertino is a high wildfire risk area.</p>", unsafe_allow_html=True)
     if st.button("Open Stevens Creek Hub", key="btn_stevens"):
         st.info("Entering the Stevens Creek Corridor Community Board")
+zone_col3, zone_col4 = st.columns(2)
+with zone_col3:
+    st.markdown("<h3 style='font-family: Arial, sans-serif; color: #1f2937; margin-top: 15px;'>Central Cupertino</h3>", unsafe_allow_html=True)
+    st.warning("Medium Risk")
+    st.markdown("<p style='font-family: Arial, sans-serif; color: #4b5563; font-size: 14px; margin-top: 5px;'>The residential and commercial core, including Downtown Cupertino. This area has a lower direct fire risk but it is important for coordinating evacuation routes and smoke safety.</p>", unsafe_allow_html=True)
+    if st.button("Open Central Hub", key="btn_central"):
+        st.info("Entering the Central Cupertino Community Board")
+with zone_col4:
+    st.markdown("<h3 style=' font-family: Arial, sans-serif; color: #1f2937; margin-top: 15px;'>North Cupertino</h3>", unsafe_allow_html=True)
+    st.warning("Medium Risk")
+    st.markdown("<p style='font-family: Arial, sans-serif; color: #4b5563; font-size: 14 px; margin-top: 5px;'>The northern neighborhoods bordering Sunnyvale and Santa Clara. Lower direct wildfire threats, but heavily imopacted by refional wildfire smoke and air quality hazards.</p>", unsafe_allow_html=True)
+    if st.button("Open North Hub, key="btn_north"):
+        st.info("Entering the North Cupertino Community Board")
